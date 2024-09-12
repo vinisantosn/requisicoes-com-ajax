bcd4aace2cb17197e190c676bc04a4399b907b5d
Vinicius Santos <vinisantoosk@gmail.com>
2024-09-12 17:34:37 -0300

    feat: capturando erros

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const photoProfile = document.querySelector('.profile-avatar')
+  const photoProfile = document.querySelector(".profile-avatar");
   const name = document.querySelector(".profile-name");
   const username = document.querySelector(".profile-username");
-  const repositories = document.querySelector('.repositories');
-  const followersD = document.querySelector('.followers');
-  const followingD = document.querySelector('.following');
-  const profileLink = document.querySelector('.profile-link');
-
-  
+  const repositories = document.querySelector(".repositories");
+  const followersD = document.querySelector(".followers");
+  const followingD = document.querySelector(".following");
+  const profileLink = document.querySelector(".profile-link");
 
   const endpoint = "https://api.github.com/users/vinisantosn";
 
@@ -16,12 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
       return response.json();
     })
     .then(function (json) {
-        photoProfile.src =json.avatar_url;
+      photoProfile.src = json.avatar_url;
       name.innerText = json.name;
       username.innerText = `@${json.login}`;
       repositories.innerText = json.public_repos;
-      followersD.innerText= json.followers;
+      followersD.innerText = json.followers;
       followingD.innerText = json.following;
-      profileLink.href = json.html_url
+      profileLink.href = json.html_url;
+    })
+    .catch(function (erro) {
+      alert("Verificar endpoint.")
     });
 });
