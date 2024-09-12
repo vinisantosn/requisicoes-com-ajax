0d3a3a7b20a4ed5bec24d29686e06d26890351f0
Vinicius Santos <vinisantoosk@gmail.com>
2024-09-12 17:25:33 -0300

    feat: consumindo meu github com Fetch

--- main.js
+++ main.js
@@ -1,72 +1,27 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   document
-//     .getElementById("btn-buscar-cep")
-//     .addEventListener("click", function () {
-//       const xhttp = new XMLHttpRequest();
-//       const cep = document.getElementById("cep").value;
-//       const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+document.addEventListener("DOMContentLoaded", function () {
+    const photoProfile = document.querySelector('.profile-avatar')
+  const name = document.querySelector(".profile-name");
+  const username = document.querySelector(".profile-username");
+  const repositories = document.querySelector('.repositories');
+  const followersD = document.querySelector('.followers');
+  const followingD = document.querySelector('.following');
+  const profileLink = document.querySelector('.profile-link');
 
-//       xhttp.open("GET", endpoint);
-//       xhttp.send();
-//     });
-// });
+  
 
-$(document).ready(function () {
-  $("#cep").mask("00000-000");
+  const endpoint = "https://api.github.com/users/vinisantosn";
 
-  $("#btn-buscar-cep").click(function () {
-    const cep = $("#cep").val();
-    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-    const botao = $(this);
-    $(botao).find("i").addClass("d-none");
-    $(botao).find("span").removeClass("d-none");
-
-
-// UTILIZANDO AJAX
-    // $.ajax(endpoint).done(function (resposta) {
-    //   const logradouro = resposta.logradouro;
-    //   const bairro = resposta.bairro;
-    //   const cidade = resposta.localidade;
-    //   const estado = resposta.uf;
-    //   const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-    //   $('#endereco').val(endereco)
-
-    //   setTimeout(function(){
-
-    //       $(botao).find('i').removeClass('d-none');
-    //       $(botao).find('span').addClass('d-none');
-    //   },3000);
-    // });
-
-    fetch(endpoint)
-      .then(function (resposta) {
-        return resposta.json();
-      })
-      .then(function (json) {
-        const logradouro = json.logradouro;
-        const bairro = json.bairro;
-        const cidade = json.localidade;
-        const estado = json.uf;
-        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-        $("#endereco").val(endereco);
-      })
-      .catch(function (erro) {
-        alert(
-          "Ocorreu um erro ao buscar o endereço, tente novamente mais tarde."
-        );
-      })
-      .finally(function () {
-        setTimeout(function () {
-          $(botao).find("i").removeClass("d-none");
-          $(botao).find("span").addClass("d-none");
-        }, 1000);
-      });
-  });
-
-  $('#formulario-pedido').submit(function(evento){
-    evento.preventDefault()
-    if($('#nome').val().length == 0){
-        throw new Error('Digite o nome')
-    }
-  })
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+        photoProfile.src =json.avatar_url;
+      name.innerText = json.name;
+      username.innerText = `@${json.login}`;
+      repositories.innerText = json.public_repos;
+      followersD.innerText= json.followers;
+      followingD.innerText = json.following;
+      profileLink.href = json.html_url
+    });
 });
